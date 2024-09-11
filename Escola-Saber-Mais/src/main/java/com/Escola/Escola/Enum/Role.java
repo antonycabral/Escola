ae6e4d87bfa63e7a8a276284899f7c8134b8d0fa
LeticiaMascarenhas01 <lsmascarenhas20@gmail.com>
2024-09-10 22:23:36 -0300

    AAAA

---
+++ Escola-Saber-Mais/src/main/java/com/Escola/Escola/Enum/Role.java
@@ -0,0 +1,9 @@
+package com.Escola.Escola.Enum;
+
+public enum Role {
+    PROFESSOR, 
+    ALUNO,
+    PAI,
+    COORDENADOR,
+    DIRETOR
+}
--- Escola-Saber-Mais/src/main/java/com/Escola/Escola/model/Usuario.java
+++ Escola-Saber-Mais/src/main/java/com/Escola/Escola/model/Usuario.java
@@ -1,14 +1,71 @@
 package com.Escola.Escola.model;
 
+import com.Escola.Escola.Enum.Role;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name= "tb_usuario")
 public class Usuario {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Long id; 
+    private String nome; 
+    private String email;
+    private String senha;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public Usuario() {
+    }
+
+    
 }
