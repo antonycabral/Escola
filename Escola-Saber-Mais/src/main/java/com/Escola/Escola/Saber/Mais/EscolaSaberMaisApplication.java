0795b93721f99f27de652029aafbce06eef9b2bc
LeticiaMascarenhas01 <lsmascarenhas20@gmail.com>
2024-09-10 21:58:34 -0300

    Adicionando projeto base

---
+++ Escola-Saber-Mais/src/main/java/com/Escola/Escola/Saber/Mais/EscolaSaberMaisApplication.java
@@ -0,0 +1,13 @@
+package com.Escola.Escola.Saber.Mais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscolaSaberMaisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EscolaSaberMaisApplication.class, args);
+	}
+
+}
---
+++ Escola-Saber-Mais/src/test/java/com/Escola/Escola/Saber/Mais/EscolaSaberMaisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Escola.Escola.Saber.Mais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscolaSaberMaisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
